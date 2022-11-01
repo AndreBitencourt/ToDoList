f4dcc8c12ebed2b933e6d3433b59af47767f4b7e
André Bitencourt <35121206+AndreBitencourt@users.noreply.github.com>
2022-11-01 09:50:19 -0300

    Add files via upload
    Arquivo atualizado para trabalho

--- index.js
+++ index.js
@@ -1,63 +1,109 @@
-const texto = document.querySelector('input');
-const btnInsert = document.querySelector('.divInsert button')
-const btnDeleteAll = document.querySelector('.header button')
-const ul = document.querySelector('ul')
+//Crio uma constante que lê os seletores input
+//Atividade
+const texto = document.querySelector('.txtInputTarefa input');
+//Categoria
+const categoria = document.querySelector('.txtInputCategoria input');
+//hora
+const hora = document.querySelector('.txtInputHora input');
 
-var itensDB = []
+let edita = false;
 
-btnDeleteAll.onclick = () => {
-  itensDB = []
-  updateDB()
+//Pego o clique no + para editar texto do modal
+const btnAbreModal = document.getElementById('addTarefa');
+btnAbreModal.onclick = () => {
+  document.getElementById('modal-titulo').innerHTML = 'Nova Tarefa';
+  document.getElementById('salvar').innerHTML = 'Salvar';
+  edita = false;
 }
 
+//Lê dentro da class .divInsert o atributo button
+// const btnInsert = document.querySelector('.divInsert button');
+const btnInsert = document.querySelector('.modal-footer button');
+
+//Delete All - Lê dentro da classe header o atributo button
+//const btnDeleteAll = document.querySelector('.header button');
+
+//Leio todos as listas (atentar para que se usar outra lista ordenada agrupar por classe ou dentro de algum id)
+const ul = document.querySelector('ul');
+
+//Inicaliza a variável ítensDB como um array
+//Observar que ela fica como var para ser usada no escopo de funções posteriores
+var itensDB = []
+
+//Ação de deletar todos os registros
+//O delete apenas registra o valor vazio no array
+/*btnDeleteAll.onclick = () => {
+  itensDB = []
+  updateDB();
+}*/
+
+//Ação ao pressionar tecla. apenas tecla enter está configurada
 texto.addEventListener('keypress', e => {
   if (e.key == 'Enter' && texto.value != '') {
     setItemDB()
   }
 })
 
+
+//Ação ao clicar botão btnInsert
 btnInsert.onclick = () => {
+  //Analiso se o conteúdo do botão é diferente de vazio
   if (texto.value != '') {
-    setItemDB()
+    setItemDB();//Seta itemDB
+    //Verifico se é update, caso seja apago o item anterior    
+    if (edita) {
+      const i = document.getElementById('id-edita').value;      
+      removeItem(i);
+    }    
   }
 }
 
+//Inserir ítem no LS
 function setItemDB() {
-  if (itensDB.length >= 20) {
-    alert('Limite máximo de 20 itens atingido!')
+  if (itensDB.length >= 20) {//Limita em 20 as atividades
+    alert('Limite máximo de 20 itens atingido!');
     return
   }
 
-  itensDB.push({ 'item': texto.value, 'status': '' })
-  updateDB()
+  //Adiciona um ítem ao array  
+  itensDB.push({ 'item': texto.value, 'categoria': categoria.value, hora: hora.value, 'status': '' })
+  updateDB();
 }
 
+
+//Atualizo Local Storage com os dados armazenados no itensDB
 function updateDB() {
   localStorage.setItem('todolist', JSON.stringify(itensDB))
-  loadItens()
+  loadItens();
 }
 
 function loadItens() {
   ul.innerHTML = "";
   itensDB = JSON.parse(localStorage.getItem('todolist')) ?? []
   itensDB.forEach((item, i) => {
-    insertItemTela(item.item, item.status, i)
+    insertItemTela(item.item, item.categoria, item.hora, item.status, i);
   })
 }
 
-function insertItemTela(text, status, i) {
-  const li = document.createElement('li')
-  
+//Inserir ítem na tela
+function insertItemTela(text, categoria, hora, status, i) {
+
+  const li = document.createElement('li');
+
   li.innerHTML = `
     <div class="divLi">
       <input type="checkbox" ${status} data-i=${i} onchange="done(this, ${i});" />
       <span data-si=${i}>${text}</span>
-      <button onclick="editaItem(${i})" data-i=${i}><i class='bx bx-edit'></i></button>
+      <span>${categoria}</span>
+      <span>${hora}</span>
+      <button onclick="editaItem(${i})" data-bs-toggle="modal" data-bs-target="#modal" data-i=${i}><i class='bx bx-edit'></i></button>
       <button onclick="removeItem(${i})" data-i=${i}><i class='bx bx-trash'></i></button>
     </div>
-    `
-  ul.appendChild(li)
+    `;
+  //Adiciona uma ul ao final da lista
+  ul.appendChild(li);
 
+  //Linha riscada - Atividade Concluída
   if (status) {
     document.querySelector(`[data-si="${i}"]`).classList.add('line-through')
   } else {
@@ -70,28 +116,52 @@ function insertItemTela(text, status, i) {
 function done(chk, i) {
 
   if (chk.checked) {
-    itensDB[i].status = 'checked' 
+    itensDB[i].status = 'checked'
   } else {
-    itensDB[i].status = '' 
+    itensDB[i].status = ''
   }
 
   updateDB()
 }
 
 function removeItem(i) {
-  itensDB.splice(i, 1)
+  itensDB.splice(i, 1)//Alterar conteúdo do array e reordenar o mesmo
   updateDB()
 }
 
-loadItens()
+function editaItem(i) {
+  //O modal é aberto pelo button
+  //Insiro as informações já preenchidas ao modal
+  //const recTxtTarefa = document.querySelector('.txtInputTarefa input');
+  const dadosPreenchidos = itensDB[i];
+  //alert(dadosPreenchidos.item);
+  //.categoria
+  //.hora  
+  document.querySelector('.txtInputTarefa input').value = dadosPreenchidos.item;
+  document.querySelector('.txtInputCategoria input').value = dadosPreenchidos.categoria;
+  document.querySelector('.txtInputHora input').value = dadosPreenchidos.hora;
+  document.getElementById('id-edita').value = i;
+  //Altero os texto do modal
+  document.getElementById('modal-titulo').innerHTML = 'Editar Tarefa';
+  document.getElementById('salvar').innerHTML = 'Atualizar Tarefa';
+  //itensDB.splice(i, 1)
+  //updateDB()
 
+  edita = true;
+
+}
+
+loadItens();//Inicaliza os registros
+
+
+//DATA E RELÓGIO 
 var timeDisplay = document.getElementById('relogio');
 
-        function refreshTime() {
+function refreshTime() {
 
-            var dateString = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
-            var formattedString = dateString.replace(', ', ' - ');
-            timeDisplay.innerHTML = formattedString;
-        }
+  var dateString = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+  var formattedString = dateString.replace(', ', ' - ');
+  timeDisplay.innerHTML = formattedString;
+}
 
-        setInterval(refreshTime, 1000);
+setInterval(refreshTime, 1000);
